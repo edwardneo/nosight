8f6d4a897713b601a1fb4c4066d866dcf0c28ce7
Edward Lee <edwardneo@berkeley.edu>
2022-10-16 12:12:03 -0700

    Updated view

--- frontend/src/components/view.js
+++ frontend/src/components/view.js
@@ -1,26 +1,45 @@
 import './view.css';
+import { useState, useEffect } from 'react';
 
 function View() {
-  return (
-    <div className="view">
-      {getImages()}
-    </div>
-  );
-}
+  const [ imageArr, setImageArr ] = useState([]);
 
-function getImages() {
-  fetch("https://us-central1-nosight-c105e.cloudfunctions.net/app/getImages")
+  const getImages = () => {
+    fetch("https://us-central1-nosight-c105e.cloudfunctions.net/app/getImages")
     .then(res => res.json())
     .then(res => {
-      return (res.images.map((image, i) => 
-        (
-          <div key={i}>
-            <img className="close" src="./x.svg" alt="Close"></img>
-            <img src={image.uri} alt="Filler"></img>
-          </div>
-        )
-      ))
+      const images = [];
+      res.images.forEach((image, i) => {
+          images.push((
+            <div className="content" key={i}>
+              <h1 className="close">X</h1>
+              <img className="img" src={image.data.uri} alt="Filler" />
+            </div>
+          ));
+          
+          if (image.data.uri2 !== undefined) {
+            images.push((
+              <div className="content" key={i}>
+                <h1 className="close">X</h1>
+                <img className="img" src={image.data.uri2} alt="Filler" />
+              </div>
+            ))
+          }
+        }
+      );
+      setImageArr(images);
     }).catch(err => console.error(err));
+  }
+
+  useEffect(() => {
+    getImages();
+  }, []);
+
+  return (
+    <div className="view">
+      {imageArr}
+    </div>
+  );
 }
 
 export default View;
